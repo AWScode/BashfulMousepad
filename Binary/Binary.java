767e60996a988407a00de87cf6d51f00ba7dcff2
HuiYi Cai <huiyi_cai@m013358.aw.org>
2017-12-06 13:23:09 -0800

    ok

---
+++ Binary/Binary.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Binary {
+      public static void main(String[] arg){
+      Scanner in= new Scanner(System.in);
+      System.out.println("Please give the first binary number.");
+      String a = in.nextLine();
+      int numbera = Integer.parseInt(a);
+System.out.println(numbera);
+      Scanner in=new Scanner(System.in);
+      System.out.println("Please give the second binary number.")
+      String b = in.nextLine();
+      int numberb = Integer.parseInt(b);
+      System.out.println(numberb);
+    }
+  }
