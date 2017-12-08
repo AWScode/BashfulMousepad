2185be10b621ce97354c6467fa0ecb5a5cde4766
HuiYi Cai <huiyi_cai@m013358.aw.org>
2017-12-08 13:53:50 -0800

    ok

--- Binary/Binary.java
+++ Binary/Binary.java
@@ -1,15 +1,26 @@
 import java.util.*;
 public class Binary {
       public static void main(String[] arg){
-      Scanner in= new Scanner(System.in);
-      System.out.println("Please give the first binary number.");
-      String a = in.nextLine();
-      int numbera = Integer.parseInt(a);
-System.out.println(numbera);
-      Scanner in=new Scanner(System.in);
-      System.out.println("Please give the second binary number.")
-      String b = in.nextLine();
-      int numberb = Integer.parseInt(b);
-      System.out.println(numberb);
-    }
+      Scanner in1= new Scanner(System.in);
+      System.out.println("Please give the first binary number."); //ask for typing the first binary number for later calculation//
+      String a = in1.nextLine(); //input of the string version of the first binary number//
+
+      Scanner in2=new Scanner(System.in);
+      System.out.println("Please give the second binary number."); //ask for typing the second binary number for later calculation//
+      String b = in2.nextLine(); //input of the string version of the second binary number//
+
+      int numbera []=new int[8]; //the array for storing the input a in 8 digits
+      for (int i=0; i<8; i++) {
+        if(i<a.length()){
+          int aa =a.length()-1;
+          int numbera1 = Integer.parseInt(a.substring(aa-i,a.length()-i));
+          System.out.println(numbera1);
+          numbera[i]=numbera1;
+        }
+        else{
+          numbera[i]=0;
+        }
+      }
+  System.out.println(Binary.toint(numbera));
   }
+}
