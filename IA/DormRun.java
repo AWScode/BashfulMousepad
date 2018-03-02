c1fd4f1288c82025fa13e7650c64358a9454ece6
HuiYi Cai <huiyi_cai@m013358.aw.org>
2018-03-02 12:18:48 -0800

    okay

---
+++ IA/DormRun.java
@@ -0,0 +1,33 @@
+//这个class会和另一个class 一起合作。这个是总的class，需要计算average 的一个class，
+import java.util.*;
+public class DormRun{
+  public static void main (String[] arg){
+    File file=new File(" ");//need locations(for students recomendation)
+    BufferedReader reader=null;
+    String temp=null;
+    int line=1;
+    File file1=new File(" ");//need locations(for record)
+    BufferedReader reader=null;
+    String temp=null;
+    int line=2;
+    public void calRank(){
+      //It will calculate the ranking
+    }
+    Scanner in= new Scanner(System.in);
+    System.out.println("Please enter the month.");
+    String a = in.nextLine();
+
+    Scanner in1= new Scanner(System.in);
+    System.out.println("Please enter the day.");
+    String b = in.nextLine();
+
+// Create an array list/hashmap for storing the date dorm went to 
+  public // method for clearing  the hashmap
+
+    // If statement for days
+    //target
+    If(a.equals("September"||"January"))
+    //
+
+  }
+}
